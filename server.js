99995833743c97598cc81709f63e69f5b6a0e326
Glitch (headerparser69) <none>
2018-03-26 08:55:55 +0000

    🏇🐇 Checkpoint ./server.js:779516/359

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+
+//Get CALL to return JSON thats format natural and unix date
+app.get("/API/header", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+
+
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
   
