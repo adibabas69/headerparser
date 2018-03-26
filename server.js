b2f1c9afcf5a28f57eeaff567c70662ef4a399a6
Glitch (headerparser69) <none>
2018-03-26 09:28:54 +0000

    👣🍬 Checkpoint ./server.js:779516/120 ./package.json:779516/1260

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Basic required import for NodeJS
-var express = require('express')
+var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var useragent = require('express-useragent');
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(useragent.express());
 
 //API url
 var api = 'api/whoami/';
@@ -15,10 +17,10 @@ var api = 'api/whoami/';
 
 
 //Get CALL to return JSON thats format natural and unix date
-app.get(api, function(req, res) {
+app.get(api, function(req, res, next) {
   
   var language = req.acceptsLanguages();
-  var software = req.get('User-Agent');
+  var software = req.useragent;
   var ipaddress = req.ip;
   
   res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software });
@@ -30,4 +32,4 @@ app.get(api, function(req, res) {
 const listener = app.listen(process.env.PORT, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
   
-})
+});
